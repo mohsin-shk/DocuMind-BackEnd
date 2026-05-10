c26810cd0431e98fbb02cf9031d5810cad6d95db
moshin-shk <abushaikh2002.19@gmail.com>
2026-05-10 21:22:09 +0530

    api response and api error handling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import dotenv from "dotenv"
 import connectDB from "./configs/db.js"
+import { app } from "./app.js"
 
 dotenv.config({
     path:"./env"
 })
 
 connectDB()
+.then(()=>{
+    app.on((err)=>{
+        console.log(`Server Connection Failed:`, err)
+    });
+
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`🛠️ Server is listening on port: ${process.env.PORT || 8000}`)
+    })
+})
+.catch((err)=>{
+    console.log("Mongo DB connection failed!!!:",err)
+})
